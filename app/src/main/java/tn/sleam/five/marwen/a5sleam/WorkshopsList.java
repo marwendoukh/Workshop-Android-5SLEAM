f4d00c29f82f4fc7ae027a6208cce2684822631d
Marwen Doukh <marwen.doukh@gmail.com>
2017-10-06 19:38:49 +0100

    workshop list

--- app/src/main/java/tn/sleam/five/marwen/a5sleam/WorkshopsList.java
+++ app/src/main/java/tn/sleam/five/marwen/a5sleam/WorkshopsList.java
@@ -6,22 +6,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import tn.sleam.five.marwen.a5sleam.recyclerview.Main2Activity;
+import tn.sleam.five.marwen.a5sleam.recyclerview.ChooseCountry;
 
-public class MainActivity extends AppCompatActivity {
+public class WorkshopsList extends AppCompatActivity {
 
     Button goToRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_workshops_list);
 
+
+        // RecyclerView workshop
         goToRecyclerView = (Button) findViewById(R.id.go_to_recyclerview_course);
-
         goToRecyclerView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), Main2Activity.class));
+                startActivity(new Intent(getApplicationContext(), ChooseCountry.class));
             }
         });
     }
